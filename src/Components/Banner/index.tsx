3fe851a1b07ceebb6103349c8fe455fc9c14a8e2
Byanew23 <borislav.yanev23@gmail.com>
2024-06-11 18:55:25 +0300

    update banner and fonts

--- src/Components/Banner/index.tsx
+++ src/Components/Banner/index.tsx
@@ -4,15 +4,36 @@ import { Application } from '@splinetool/runtime'
 
 const splineLink = 'https://prod.spline.design/2q3tJzSrpKSCMRpl/scene.splinecode'
 export const Banner = () => {
+    const canvasRef = React.useRef<HTMLCanvasElement>(null)
+    const wrapperRef = React.useRef<HTMLDivElement>(null)
+
+    function preventScroll(e: any) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        return false;
+    }
+
+    function disable() {
+        canvasRef?.current?.addEventListener('wheel', preventScroll, { passive: false });
+    }
+
+    function enable() {
+        canvasRef?.current?.removeEventListener('wheel', preventScroll);
+    }
+
     // const canvas = document.getElementById('canvas3d')
     React.useEffect(() => {
         const canvas = document.getElementById('canvas3d')
 
         const spline = new Application(canvas as any)
         spline.load(splineLink)
+
     }, [])
 
-    return <div>
-        <canvas id='canvas3d' />
+
+
+    return <div ref={wrapperRef}>
+        <canvas id='canvas3d' ref={canvasRef} onMouseEnter={() => disable()} onMouseLeave={() => enable()} />
     </div>
 }
